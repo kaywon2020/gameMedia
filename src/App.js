7dbf057565f798d308de47bc9a3e5e6030db234c
Kwonyoungoh <rnjsduddh05@gmail.com>
2020-09-15 04:18:21 +0900

    test2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          보형이 자지 5cm
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          자지
         </a>
       </header>
     </div>
